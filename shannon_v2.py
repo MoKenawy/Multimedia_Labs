80ccbda0717352bd5d88f2382836c1fb6af989e4
moken <moken@LAPTOP-IH3NAIAC>
2024-05-31 16:22:59 +0300

    Changed to work for any input symbols.

--- shannon_v2.py
+++ shannon_v2.py
@@ -6,12 +6,14 @@
 ## output (encoded data)
 
 
-def shannon_encode(source_stats, data):
-    # sort desc
-    list = []
-    # list.sort(key= lambda x : , reverse=True)
-    source_stats.sort()
-
+def shannon_encode(data, code_table):
+    encoded_data = []
+    data = data.split(" ")
+    for char in data:
+        for symbol, code in code_table:
+            if char == symbol:
+                encoded_data.append(code)
+    return encoded_data
 
 
 def divide(source_stats, code = "" , code_table = []):
@@ -31,25 +33,30 @@ def divide(source_stats, code = "" , code_table = []):
             return divide(source_stats[i:], code + "0", code_table[:]) + divide(source_stats[:i], code + "1", code_table[:])
 
 if __name__ == "__main__":
-    data = "AAABBFFGD"
-    source_stats = [
-        ('E', 1000),
-        ('D',800),
-        ('C', 400),
-        ('G', 400),
-        ('B', 200),
-        ('A', 100),
-        ('F', 100)
-    ]
+    data = "25 65 170"
+    # source_stats = [
+    #     ('E', 1000),
+    #     ('D',800),
+    #     ('C', 400),
+    #     ('G', 400),
+    #     ('B', 200),
+    #     ('A', 100),
+    #     ('F', 100)
+    # ]
 
+    source_stats = [
+        ('40', 600),
+        ('30', 200),
+        ('65', 200),
+        ('10', 100),
+        ('25', 100),
+        ('150', 100),
+        ('170', 100),
+        ('250', 100)
+    ]
 
     code_table =  divide(source_stats)
     print(code_table)
-
-    encoded_data = []
     #encode
-    for char in data:
-        for symbol, code in code_table:
-            if char == symbol:
-                encoded_data.append(code)
+    encoded_data = shannon_encode(data, code_table)
     print(f"encoded data: {encoded_data}")
